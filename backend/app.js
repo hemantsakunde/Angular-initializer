5361a5e097742a97e4724b0e2ca82510eedfba2c
hemantsakunde <hemantsakunde7@gmail.com>
2025-10-24 21:21:48 +0530

    Handled CORS

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "https://angular-initializer-aa079.web.app/",
+    origin: "https://angular-initializer-aa079.web.app",
     methods: ["GET", "POST"],
   })
 );
